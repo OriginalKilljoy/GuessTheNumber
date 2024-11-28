3d70b12ab92c72e950647967d63fe0398656d0e0
OriginalKilljoy <162512500+OriginalKilljoy@users.noreply.github.com>
2024-11-28 15:01:22 +0100

    Add project files.

---
+++ GuessTheNumber/Program.cs
@@ -0,0 +1,36 @@
+﻿//Lag et program hvor brukeren skal gjette et tilfeldig tall mellom 1-100
+//Hver gang brukeren skriver inn et tall vil man få svar fra programmet om man må høyere eller lavere, helt til man gjetter riktig tall. Da skal man få spørsmål om man vil spille på nytt
+
+Random randomNumber = new Random();
+var correctNumber = randomNumber.Next(1,101);
+
+
+Console.WriteLine("Hello! Guess a number between 1-100");
+GuessingGame();
+
+void GuessingGame()
+{
+    var userGuess = Console.ReadLine();
+    int guess = int.Parse(userGuess);
+    NumberChecker(guess);
+}
+
+
+
+void NumberChecker(int guess)
+{
+    if (guess < correctNumber)
+    {
+        Console.WriteLine("Higher;)");
+        GuessingGame();
+    }
+    else if (guess > correctNumber)
+    {
+        Console.WriteLine("Lower;)");
+        GuessingGame();
+    }
+    else
+    {
+        Console.WriteLine("Correct!!");
+    }
+}
